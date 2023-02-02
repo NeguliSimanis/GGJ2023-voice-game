fc6256423ecc10950fccab77a51bcc0171e7b711
NeguliSimanis <simanism@inbox.lv>
2023-02-02 20:22:16 +0200

    start

---
+++ 2023 GGJ AUDIO GAME/Assets/Scripts/VoiceMovement.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.Windows.Speech;
+
+public class VoiceMovement : MonoBehaviour
+{
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+    private void Start()
+    {
+        actions.Add("forward", Forward);
+        actions.Add("up", Up);
+        actions.Add("down", Down);
+        actions.Add("back", Back);
+        actions.Add("right", Right);
+        actions.Add("left", Left);
+        actions.Add("play", Play);
+
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += ProcessSpeech;
+        keywordRecognizer.Start();
+    }
+
+    private void ProcessSpeech(PhraseRecognizedEventArgs speech)
+    {
+        Debug.Log(speech.text);
+        actions[speech.text].Invoke();
+    }
+
+    private void Forward()
+    {
+        transform.Translate(1, 0, 0);
+    }
+
+    private void Back()
+    {
+        transform.Translate(-1, 0, 0);
+    }
+
+    private void Up()
+    {
+        transform.Translate(0, 1, 0);
+    }
+
+    private void Down()
+    {
+        transform.Translate(0, -1, 0);
+    }
+
+    private void Right()
+    {
+        transform.Translate(1, 0, 0);
+    }
+
+    private void Left()
+    {
+        transform.Translate(-1, 0, 0);
+    }
+
+    private void Play()
+    {
+        Debug.Log("plaay");
+    }
+}
